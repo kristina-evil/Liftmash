e488a3dcaa6ad5917b94fb8d3597a8f71c1c48ea
Кристина Тищенко <kris-marin@bk.ru>
2021-06-08 11:32:57 +0300

    Added solution, entity for materials

---
+++ Liftmash/Liftmash.Domain.Entities/Materials.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Liftmash.Domain.Entities
+{
+    public class Materials
+    {
+        public long Id { get; set; }
+
+        [Column("Код")]
+        public string Code { get; set; }
+
+        [Column("Артикул")]
+        public string Article { get; set; }
+
+        [StringLength(200)]
+        [Column("Наименование")]
+        public string Name { get; set; }
+
+        [Column("Единица измерения")]
+        public string Unit { get; set; }
+
+        [Column("Количество")]
+        public long Count { get; set; }
+
+        [Column("Цена",TypeName = "money")]
+        public decimal Price { get; set; }
+    }
+}
